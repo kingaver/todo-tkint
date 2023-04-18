d303103cea4a999bec4738b3d334a1931dcc0934
kingaver <kingaver@msu.edu>
2023-04-18 15:29:32 -0400

    Added Elements
    Added a frame for the new task elements and a text element

--- app.py
+++ app.py
@@ -3,4 +3,15 @@ import tkinter as tk
 root = tk.Tk()
 root.geometry('500x500')
 
+# Title at the top of the window
+title = tk.Label(master=root, text="To-Do", font='Calibri 24 bold', pady=20)
+title.pack()
+
+# Add frames for the new task entry / task display
+top_frame = tk.Frame(master=root)
+
+new_task = tk.Text(master=top_frame)
+top_frame.pack()
+new_task.pack()
+
 root.mainloop()
