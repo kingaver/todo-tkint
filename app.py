c75617cc1a14dbd65a9d4cb196ea223fe45731e5
kingaver <kingaver@msu.edu>
2023-04-19 08:39:17 -0400

    Updated addtask
    addtask now inserts the value of the entry element when button is pressed

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import tkinter as tk
 
-def addtask(lb):
-    lb.insert(0,'Hello')
+def addtask(lb, task_string):
+    lb.insert(0,task_string.get())
 
 root = tk.Tk()
 root.geometry('500x500')
+
+task_string = tk.StringVar()
 
 # Title at the top of the window
 title = tk.Label(master=root, text="To-Do", font='Calibri 24 bold', pady=20)
@@ -15,8 +17,8 @@ bot_frame = tk.Frame(master=root, width=450, height=300, bg="white")
 bot_frame.pack_propagate(False)
 
 # Add elements for task entry
-new_task = tk.Entry(master=top_frame, width=50)
-button = tk.Button(master=top_frame, width=10, padx=10, text="Add Task", command=lambda: addtask(lb))
+new_task = tk.Entry(master=top_frame, width=50, textvariable=task_string)
+button = tk.Button(master=top_frame, width=10, padx=10, text="Add Task", command=lambda: addtask(lb, task_string))
 lb = tk.Listbox(master=root, width=80, height=30, font='Calibri 24 bold')
 
 # Pack elements into window
