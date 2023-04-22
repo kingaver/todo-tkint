020a1f876c975f3bc2f01f976665e07865218259
kingaver <kingaver@msu.edu>
2023-04-22 12:23:45 -0400

    Added Delete function
    'delete' now checks if the current selection is the last item in the listbox, and changes current selection to the new last item in the box.

--- app.py
+++ app.py
@@ -5,8 +5,17 @@ def addtask(task_string, tasks):
     task_string.set('')
     Tasks.set(tasks)
 
-def check(event):
-    print(check)
+def delete(event):
+    tasks_length = len(tasks)
+    current_idx = lb.curselection()[0]
+    del tasks[current_idx]
+    Tasks.set(tasks)
+    if current_idx == tasks_length - 1:
+        lb.select_clear(0, tk.END)
+        lb.select_set(current_idx - 1)
+    else:
+        lb.select_clear(0, tk.END)
+        lb.select_set(current_idx)
 
 root = tk.Tk()
 root.geometry('500x500')
@@ -26,15 +35,19 @@ Tasks = tk.StringVar(value=tasks)
 
 # Add elements for task entry
 new_task = tk.Entry(master=top_frame, width=50, textvariable=task_string)
-button = tk.Button(master=top_frame, width=10, padx=10, text="Add Task", command=lambda: addtask(task_string, tasks))
+button = tk.Button(master=top_frame, width=10, padx=10, text="Add Task", command=lambda : addtask(task_string, tasks))
+button_del = tk.Button(master=top_frame, width=10, padx=10, text="Delete Task", command=delete)
 lb = tk.Listbox(master=root, width=30, height=10, font='Calibri 16 bold',listvariable=Tasks)
 new_task.bind('<Return>', lambda event: addtask(task_string, tasks))
+lb.bind('<BackSpace>', delete)
+
 
 # Pack elements into window
 title.pack()
 top_frame.pack()
 new_task.pack(fill=tk.X, side=tk.LEFT, expand=True)
 button.pack(padx=10)
+button_del.pack(padx=10)
 lb.pack(pady=20)
 
 root.mainloop()
