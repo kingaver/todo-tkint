e6ef3b9d141119ea75374543bbc07dddbe1ec20e
kingaver <kingaver@msu.edu>
2023-04-23 20:25:54 -0400

    Added Priority Menu
    Added priority menu, have not connected it to addtask yet.

--- app.py
+++ app.py
@@ -18,6 +18,7 @@ def delete():
         lb.select_set(current_idx)
 
 root = tk.Tk()
+options = ["Low", "Medium", "High"]
 root.geometry('500x500')
 
 task_string = tk.StringVar()
@@ -32,11 +33,13 @@ bot_frame.pack_propagate(False)
 
 tasks = []
 Tasks = tk.StringVar(value=tasks)
+defaultoption = tk.StringVar(value=options[0])
 
 # Add elements for task entry
 new_task = tk.Entry(master=top_frame, width=50, textvariable=task_string)
 button = tk.Button(master=top_frame, width=10, padx=10, text="Add Task", command=lambda : addtask(task_string, tasks))
 button_del = tk.Button(master=top_frame, width=10, padx=10, text="Delete Task", command=delete)
+priority_menu = tk.OptionMenu(top_frame, defaultoption, *options)
 lb = tk.Listbox(master=root, width=30, height=10, font='Calibri 16 bold',listvariable=Tasks)
 new_task.bind('<Return>', lambda event: addtask(task_string, tasks))
 lb.bind('<BackSpace>', lambda event: delete())
@@ -45,9 +48,10 @@ lb.bind('<BackSpace>', lambda event: delete())
 # Pack elements into window
 title.pack()
 top_frame.pack()
-new_task.pack(fill=tk.X, side=tk.LEFT, expand=True)
-button.pack(padx=10)
-button_del.pack(padx=10)
+new_task.pack()
+button.pack(fill=tk.X, side=tk.LEFT, expand=True)
+button_del.pack(fill=tk.X, side=tk.LEFT, expand=True)
+priority_menu.pack(fill=tk.X, side=tk.LEFT, expand=True)
 lb.pack(pady=20)
 
 root.mainloop()
